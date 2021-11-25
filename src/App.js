02e5e14095f22cb82648a543b646e24bd89266d0
gregor232 <ktotok@gmail.com>
2021-11-25 01:56:44 +0100

    layout changed

--- src/App.js
+++ src/App.js
@@ -37,32 +37,37 @@ const App = () => {
   };
 
   return (
-    <>
+    <div style={{ width: "50%", margin: "0 auto" }}>
       <div className="App">
-        <button variant="contained" color="secondary" onClick={onScanFile}>
+        <button
+          style={{ display: "block", margin: "0 auto" }}
+          onClick={onScanFile}
+        >
           scan qr code
         </button>
         <QrReader
           ref={qrRef}
           delay={300}
-          style={{ width: "100%" }}
+          style={{ width: "100%", height: `10px` }}
           onError={handleErrorFile}
           onScan={handleScanFile}
           legacyMode
         />
-        <h3>{scanResultFile}</h3>
+        <h3 style={{ textAlign: "center" }}>{scanResultFile}</h3>
       </div>
-      <div>
-        <h3>web cam scan</h3>
-        <QrReader
-          delay={300}
-          style={{ width: "100%" }}
-          onError={handleErrorWebCam}
-          onScan={handleScanWebCam}
-        />
-        <h3>code: {scanResultWebCam}</h3>
+      <div style={{ width: "100%", margin: "0 auto" }}>
+        <h3 style={{ textAlign: "center" }}>web cam scan</h3>
+        <div style={{ height: "50vh", width: "50vh", margin: "0 auto" }}>
+          <QrReader
+            delay={300}
+            style={{ width: "100%", margin: "0 auto" }}
+            onError={handleErrorWebCam}
+            onScan={handleScanWebCam}
+          />
+        </div>
+        <h3 style={{ textAlign: "center" }}>code: {scanResultWebCam}</h3>
       </div>
-    </>
+    </div>
   );
 };
 
