532b551cde1df904a12b7d677dd40427e491fec4
gregor232 <ktotok@gmail.com>
2021-12-02 18:04:47 +0100

    mediadevices added

--- src/components/BarcodeScanner.js
+++ src/components/BarcodeScanner.js
@@ -4,14 +4,36 @@ import Quagga from "quagga";
 const BarcodeScanner = () => {
   const [result, setResult] = useState("Not Found");
   const [data, setData] = useState("No Data");
+  console.log(MediaStreamTrack);
 
+  var backCamID = null;
+  var last_camera = null;
+  navigator.mediaDevices
+    .enumerateDevices()
+    .then(function (devices) {
+      devices.forEach(function (device) {
+        if (
+          device.kind == "videoinput" &&
+          device.label.match(/back/) !== null
+        ) {
+          backCamID = device.deviceId;
+        }
+        if (device.kind === "videoinput") {
+          last_camera = device.deviceId;
+        }
+      });
+      if (backCamID === null) {
+        backCamID = last_camera;
+      }
+    })
+    .catch(function (err) {});
   const config = {
     inputStream: {
       type: "LiveStream",
       constraints: {
         width: 640,
         height: 480,
-        facing: "environment", // or user
+        deviceId: backCamID, // or user
       },
     },
     locator: {
@@ -31,18 +53,18 @@ const BarcodeScanner = () => {
         return console.log(err);
       }
       Quagga.start();
-      Quagga.onDetected((data) => {
-        setData(data);
-      });
-      Quagga.decodeSingle(config, (result) => {
-        if (result.codeResult) {
-          setResult(result.codeResult.code);
-        } else {
-          console.log("not detected");
-        }
-      });
     });
     // Quagga.onDetected((data) => console.log("data", data));
+    Quagga.onDetected((data) => {
+      setData(data);
+    });
+    Quagga.decodeSingle(config, (result) => {
+      if (result.codeResult) {
+        setResult(result.codeResult.code);
+      } else {
+        console.log("not detected");
+      }
+    });
   }, []);
 
   return (
