9d86689c7acfc9a39c2954c3a8aa66d848893d15
gregor232 <ktotok@gmail.com>
2021-12-02 15:50:43 +0100

    onDected added

--- src/components/BarcodeScanner.js
+++ src/components/BarcodeScanner.js
@@ -1,39 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Quagga from "quagga";
 
 const BarcodeScanner = () => {
-  useEffect(() => {
-    Quagga.init(
-      {
-        inputStream: {
-          type: "LiveStream",
-          constraints: {
-            width: 640,
-            height: 480,
-            facing: "environment", // or user
-          },
-        },
-        locator: {
-          patchSize: "medium",
-          halfSample: true,
-        },
-        numOfWorkers: 2,
-        decoder: {
-          readers: ["code_128_reader"],
-        },
-        locate: true,
+  const [result, setResult] = useState("Not Found");
+  const [data, setData] = useState("No Data");
+
+  const config = {
+    inputStream: {
+      type: "LiveStream",
+      constraints: {
+        width: 640,
+        height: 480,
+        facing: "environment", // or user
       },
-      function (err) {
-        if (err) {
-          return console.log(err);
-        }
-        Quagga.start();
+    },
+    locator: {
+      patchSize: "medium",
+      halfSample: true,
+    },
+    numOfWorkers: 2,
+    decoder: {
+      readers: ["code_128_reader"],
+    },
+    locate: true,
+  };
+
+  useEffect(() => {
+    Quagga.init(config, function (err) {
+      if (err) {
+        return console.log(err);
       }
-    );
-    Quagga.onDetected((data) => console.log("data", data));
+      Quagga.start();
+    });
+    // Quagga.onDetected((data) => console.log("data", data));
+    Quagga.onDetected((data) => {
+      setData(data);
+    });
+    Quagga.decodeSingle(config, (result) => {
+      if (result.codeResult) {
+        setResult(result.codeResult.code);
+      } else {
+        console.log("not detected");
+      }
+    });
   }, []);
 
-  return <div id="interactive" className="viewport" />;
+  return (
+    <>
+      <div id="interactive" className="viewport" />
+      <div>{result}asd</div>
+      <div>{result}no data </div>
+    </>
+  );
 };
 
 export default BarcodeScanner;
